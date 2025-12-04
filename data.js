9e50babcf5a92d3c626033f6ce1b728322b87996
OrpoProlom <217503849+OrpoPro@users.noreply.github.com>
2025-12-04 19:02:30 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+// Данные для ссылок
+const linksData = [
+    { name: "YouTube", url: "https://www.youtube.com/@OrpoPro" },
+    { name: "GitHub", url: "https://github.com/OrpoPro" },
+    { name: "ModRinth", url: "https://modrinth.com/user/OrpoPro" },
+    { name: "TikTok", url: "https://www.tiktok.com/@play.icabu.net?_t=ZM-8zYRODV6ICs&_r=1" },
+    { name: "NameMc", url: "https://ru.namemc.com/profile/OrpoPro.1" }
+];
+
+// Данные для мнений
+const opinionsData = [
+    { title: "Tlauncher", text: "помойка и просто плохой лаунчер с вирусами" },
+    { title: "Legacy", text: "так себе, есть куда лучше" },
+    { title: "Java", text: "лучше чем Bedrock" },
+    { title: "Атеизм", text: "слепая вера, а большинство атеистов — вообще не атеисты" },
+    { title: "Аборты", text: "убийство человека" },
+    { title: "Слава Икабу", text: "" }
+];
